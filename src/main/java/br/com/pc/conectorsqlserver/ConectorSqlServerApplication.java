2e4e77753eac4f0c56af2d1de9f639f1d3b3c8c6
Paulo César <pc3168@gmail.com>
2023-10-17 22:56:15 -0300

    Criando o projeto.

---
+++ src/main/java/br/com/pc/conectorsqlserver/ConectorSqlServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.pc.conectorsqlserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectorSqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectorSqlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pc/conectorsqlserver/ConectorSqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pc.conectorsqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectorSqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
